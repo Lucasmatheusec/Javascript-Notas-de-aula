a0a661ffad5a762b76ccf6b677abd958762606b8
Lucas Matheus <138264742+Lucasmatheusec@users.noreply.github.com>
2023-10-30 17:31:59 -0400

    introdução ao curso + variável let
    Variáveis (typeof)

---
+++ Modulo 01/script.js
@@ -0,0 +1,14 @@
+//alert("hello world") 
+//console.log("Olá mundo") Só aparece em "inspecionar/console" no navegador
+let nome = "Lucas" //String
+let lastName = "Matheus"
+let age = "25" //Numbers
+let logado = true // true or false || E computador não vai interpretar como variável. || Variável boolean
+let list = ["Ovos", "Massa", "Corante"] //Array abre assim [] || Aqui eu chamo por número do objeto na lista
+let fullName = {nome: "Lucas", idade: 25} //Objetos abre assim {} || aqui eu chamo pelo nome da varável dentro do fullName
+
+//typeof = Para descobrir o tipo da variável
+
+console.log(`Olá ${nome} ${lastName}, como vai?`)
+console.log(list)
+console.log(fullName)
