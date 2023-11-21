b1fd377aeb9980563d20a9ab39edadb054c97b75
Lucas Matheus <138264742+Lucasmatheusec@users.noreply.github.com>
2023-11-21 19:05:33 -0400

    update

--- Modulo 02/exerciciospraticos.js
+++ Modulo 02/exerciciospraticos.js
@@ -91,7 +91,7 @@ console.log(addsquares(3, 4));
 // === Array ===
 let colors = ['Red', 'Green', "Blue"]
 console.log(colors)
-*/
+
 let ingredientes = [
     'Agua',
     'leite',
@@ -99,4 +99,76 @@ let ingredientes = [
     'trigo',
     'fermento',
 ];
+ingredientes.push('óleo de motor') //Push é um função para adicionar items em um array.
+ingredientes.pop(); // Para remover o ultimo item do array
+ingredientes.shift(); //Para remover o primeiro item do array.
+
+
+console.log(ingredientes)
+
 console.log(`Total de ingredientes ${ingredientes.length}`)
+
+ //Exercício de array
+
+let carros = ['BMW', ' Ferrari', ' Mercedes'];
+let x = 1;
+console.log('1. ' + carros[x])
+
+carros[1] = ' Audi'
+console.log('2. Lista com Audi: ' + carros)
+
+carros.push(' Volvo')
+console.log('3. Lista com Volvo:' + carros)
+
+console.log('4. Total de itens no array: ' + carros.length)
+
+let personagem = {                      //Objetos tem Propriedade
+    nome: 'lucas',
+    idade: 25,
+    pais: 'Brasil',
+    caracteristicas: {
+        forca: 5,
+        magia: 10,
+        stamina: 3,
+        belezura: 100,
+    },
+    olhos: ['Preto', 'Azul'],
+}
+console.log(personagem.nome);
+console.log(`O personagem ${personagem.nome} tem ${personagem.idade} anos e mora no ${personagem.pais}.`);
+
+console.log(personagem.caracteristicas.belezura);
+console.log(personagem.olhos[1]);
+
+personagem.caracteristicas.forca += 15;
+console.log(personagem.caracteristicas.forca);
+
+personagem.olhos.push('verde');
+console.log(personagem.olhos);
+
+
+let avatar = {
+    nome: 'lucas',
+    idade: 25,
+    carros: [
+        {modelo: 'Porsch', color: 'black'},
+        {modelo: 'Ferrari', color: 'DarkGreen'},
+    ]
+}
+avatar.carros[1].modelo
+console.log(avatar.carros[0].modelo)
+*/
+let avatar = {
+    name: 'lucas',
+    lastName: 'Matheus',
+    idade: 25,
+    nameComplete: function() {
+        return this.name + ' ' + this.lastName + ', ' + this.idade;
+        // return `${this.name} ${this.lastName}`;
+    }
+}
+console.log(avatar.name + ' ' + avatar.lastName);
+console.log(avatar.nameComplete()); //Chamando a função como se fosse global.
+ 
+//Atenção! nunca usar uma function anônima dentro do objeto porque ela perde o 'this'.
+//  () => {}    Função anônima.
