e764d398b63a4071a2409da6cf398c18ba7ad26d
Lucas Matheus <138264742+Lucasmatheusec@users.noreply.github.com>
2023-12-15 21:16:38 -0400

    update

--- Modulo 03/exerciciospraticos.js
+++ Modulo 03/exerciciospraticos.js
@@ -1,1 +1,32 @@
 // === DOM === Document Object Model
+/*function clicou() {
+    const input = document.querySelector('input');
+    if (input.getAttribute('type') == 'text'){
+        input.setAttribute('type', 'password');
+    } else {
+        input.setAttribute('type', 'text');
+    }
+}
+
+
+function clicou(){
+    const button = document.querySelector('button');
+    console.log(button.classList)
+    button.classList.remove('azul');
+    button.classList.add('nomeDaClasse')
+}
+*/
+
+
+function apertou() {
+    console.log('Apertou!');
+}
+function segurou() {
+    console.log('Segurou!');
+}
+function soltou() {
+    console.log('Soltou!');
+}
+
+const input = document.querySelector('input');
+input.addEventListener('keyup', soltou)
